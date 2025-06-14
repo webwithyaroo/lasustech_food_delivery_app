9a015188df81a5ef0ec2adb683c0290310606431
Issa-soliu Ridollahi Olawale || webwithyaroo <143751290+webwithyaroo@users.noreply.github.com>
2025-06-14 15:18:30 +0100

    Update page.tsx
    padding top

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -76,7 +76,7 @@ export default function BlogPage() {
   );
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 pt-10">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 ">
       <div className="container mx-auto px-4 py-8">
         {/* Header */}
         <div className="text-start mb-12">
